e7bea28ae161ac3e516cf5f7d4ac95b1086c61f0
Suin Kim <soo.officer@gmail.com>
2022-01-10 14:07:01 +0900

    Post 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
