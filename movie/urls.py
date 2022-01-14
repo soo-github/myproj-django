a270696729017bacd4a1cd6b4bc9ade308027b0b
Suin Kim <soo.officer@gmail.com>
2022-01-14 11:15:46 +0900

    DRF를 활용하여 Post REST API 구현

--- movie/urls.py
+++ movie/urls.py
@@ -1,5 +1,13 @@
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from movie.views import post_list, PostViewSet
 
 app_name = "movie"
 
-urlpatterns = []
+router = DefaultRouter()
+router.register("posts", PostViewSet)
+
+urlpatterns = [
+    path("post/", post_list, name="post_list"),
+    path("api/", include(router.urls)),
+]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from movie.models import Post
+from movie.serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+def post_list(request):
+    qs = Post.objects.all()
+    data = [
+        {"id": post.id,
+         "title": post.title,
+         "poster": post.poser,
+         "story": post.story}
+        for post in qs
+    ]
