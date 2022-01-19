3a35decce0081e03a968feeba54956963da1df5b
Suin Kim <soo.officer@gmail.com>
2022-01-19 09:23:59 +0900

    url.py / 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name = "accounts"
 urlpatterns = []
 
 urlpatterns += [
-    path('api/signup', SignupAPIView.as_view(), name='signup'),
+    path('api/signup/', SignupAPIView.as_view(), name='signup'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
